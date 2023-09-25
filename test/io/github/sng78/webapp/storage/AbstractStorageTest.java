badcd3efbc381c7024701c308763da16622f52d4
Serge Gorbachev <sng78@list.ru>
2023-09-25 10:46:38 +0300

    fix the path for save resume

--- test/io/github/sng78/webapp/storage/AbstractStorageTest.java
+++ test/io/github/sng78/webapp/storage/AbstractStorageTest.java
@@ -16,7 +16,7 @@ import static io.github.sng78.webapp.ResumeTestData.createResume;
 public abstract class AbstractStorageTest {
     protected final Storage storage;
     protected static final File STORAGE_DIR =
-            new File("C:\\Users\\SnG78\\IdeaProjects\\BaseJava\\storage\\");
+            new File("C:\\Users\\SnG78\\IdeaProjects\\BaseJava\\resumes\\");
     private static final String UUID_1 = "uuid1";
     private static final Resume RESUME_1 = createResume(UUID_1, "name1");
     private static final String UUID_2 = "uuid2";

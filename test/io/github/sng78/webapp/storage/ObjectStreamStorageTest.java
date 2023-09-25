25eda8521a286dd4c107940f822ac7e220fa8699
Serge Gorbachev <sng78@list.ru>
2023-09-25 14:44:39 +0300

    add hw9

--- test/io/github/sng78/webapp/storage/ObjectStreamStorageTest.java
+++
@@ -1,7 +0,0 @@
-package io.github.sng78.webapp.storage;
-
-public class ObjectStreamStorageTest extends AbstractStorageTest {
-    public ObjectStreamStorageTest() {
-        super(new ObjectStreamStorage(STORAGE_DIR));
-    }
-}

9e1cc30e2c187df0a5f43d247af6da24d09e0fb0
Serge Gorbachev <sng78@list.ru>
2023-10-12 12:23:04 +0300

    upgrade class DeadLock

---
+++ src/io/github/sng78/webapp/MyRunnable.java
@@ -0,0 +1,27 @@
+package io.github.sng78.webapp;
+
+public class MyRunnable implements Runnable {
+    private final String A;
+    private final String B;
+
+    public MyRunnable(String A, String B) {
+        this.A = A;
+        this.B = B;
+    }
+
+    @Override
+    public void run() {
+        synchronized (A) {
+            System.out.printf("%s blocked %s%n", Thread.currentThread().getName(), A);
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.printf("%s waiting for lock %s%n", Thread.currentThread().getName(), B);
+            synchronized (B) {
+                System.out.printf("%s blocked A and B%n", Thread.currentThread().getName());
+            }
+        }
+    }
+}
